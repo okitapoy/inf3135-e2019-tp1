27a9c6ab1f07d3032709b6dc4d77bf7d75cc1468
okitapoy <okitapoy@gmail.com>
2019-06-12 09:00:10 -0400

    ajout tp1.c et .gitignore

---
+++ tp1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    // test.
+    int chiffre = 55;
+    int deux = 2;
+    printf("premeier = %d et deuxieme = %d et la somme = %d ",chiffre,deux,chiffre + deux);
+    return 0;
+}
